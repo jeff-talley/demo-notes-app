35d663a04ca71397fd8510623d26a4cbb4d7272d
Jeff Talley <jeff.talley@homewoodit.com>
2021-10-03 13:37:45 -0500

    Did favicon part

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edddit <code>src/App.js</code> and save to reload.
+          Eddit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
