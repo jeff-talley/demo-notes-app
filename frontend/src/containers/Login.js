221994ee0bf0e94801a31fd615f19a997683127e
Jeff Talley <jeff.talley@homewoodit.com>
2021-10-03 21:05:48 -0500

    Finished login and logout functionality

--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+import LoaderButton from '../components/LoaderButton'
 import './Login.css'
 import { Auth } from 'aws-amplify'
 import { useAppContext } from '../lib/contextLib'
+import { useHistory } from 'react-router-dom'
+import { onError } from '../lib/errorLib'
 
 export default function Login() {
+  const history = useHistory()
   const { userHasAuthenticated } = useAppContext()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
   function validateForm() {
     return email.length > 0 && password.length > 0
@@ -17,11 +21,15 @@ export default function Login() {
   async function handleSubmit(event) {
     event.preventDefault()
 
+    setIsLoading(true)
+
     try {
       await Auth.signIn(email, password)
       userHasAuthenticated(true)
+      history.push('/')
     } catch (e) {
-      alert(e.message)
+      onError(e)
+      setIsLoading(false)
     }
   }
 
@@ -45,9 +53,16 @@ export default function Login() {
             onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
+
+        <LoaderButton
+          block
+          size="lg"
+          type="submit"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
           Login
-        </Button>
+        </LoaderButton>
       </Form>
     </div>
   )
